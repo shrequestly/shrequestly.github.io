a080ab0181599c1008e7c3b035e80049dd6b3a28
nkg447 <nikunj.gupta447@gmail.com>
2024-11-18 13:06:16 +0530

    incorporate https://github.com/nkg447/requestly-selfhosted/commit/256c19fbd64636129b313e1c184788943ede353d

--- assets/LottieAnimation-CCyRTLKv.js
+++
@@ -1,1 +0,0 @@
-import{r as o,l as s,o as c,j as u}from"./index-DWjFBnMr.js";const d=({animationName:t,animationData:e,...l})=>{const r=o.useRef();return o.useEffect(()=>{try{s.destroy(t),s.loadAnimation({name:t,container:r.current,animationData:e,renderer:"svg",loop:!0,autoplay:!0})}catch(n){c.log("Failed to load animation",t,n)}},[t,e]),u.jsx("div",{...l,ref:r})};export{d as L};
